f3ba223a5c6944842548a1dbb859faf8e539f3ed
Medliarskyi <med.uras111@gmail.com>
2024-10-14 16:49:12 +0300

    first master commit

---
+++ UnitTest55/UnitTest55.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab55/lab55.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest55
+{
+	TEST_CLASS(UnitTest55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(gcd(48, 18), 6); 
+		}
+	};
+}
---
+++ lab55/lab55.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int gcd(int n, int m, int level = 1) {
+    std::cout << "Level of recursion: " << level << ", n = " << n << ", m = " << m << std::endl;
+
+    if (m == 0) {
+        std::cout << "Depth of recursion: " << level << std::endl;  // Виведення глибини
+        return n;  // Базовий випадок
+    }
+
+    return gcd(m, n % m, level + 1);  // Рекурсивний виклик з збільшенням рівня
+}
+
+int main() {
+    int n, m;
+    std::cout << "Enter two natural numbers: ";
+    std::cin >> n >> m;
+
+    std::cout << "GSD(" << n << ", " << m << ") = " << gcd(n, m) << std::endl;
+
+    return 0;
+}
